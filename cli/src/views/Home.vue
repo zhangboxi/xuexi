a525674289d592ca35f599691999fbdaf9514097
boxi <823998314@qq.com>
2019-10-09 18:28:48 +0800

    weishane

--- cli/src/views/Home.vue
+++ cli/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo"
+         src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <span>this is test cli</span>
   </div>
