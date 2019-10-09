32952ebe19e12e2fc2d030182a19f91de6a2ca1b
boxi <823998314@qq.com>
2019-10-09 19:46:42 +0800

    xiugai

--- cli/src/main.js
+++ cli/src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import { Button } from "ant-design-vue";
 Vue.config.productionTip = false;
 
+Vue.use(Button);
 new Vue({
   router,
   store,
