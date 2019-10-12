957ad0f5ae6967a7977b60e6cc9ef4a9a3aa0a82
boxi <823998314@qq.com>
2019-10-12 10:01:34 +0800

    菜单

--- cli/src/layouts/SilderMenu.vue
+++ cli/src/layouts/SilderMenu.vue
@@ -1,14 +1,17 @@
 <template>
   <div style="width: 256px">
     <a-menu
-      :defaultSelectedKeys="['1']"
-      :defaultOpenKeys="['2']"
+      :selectedKeys="selectedKeys"
+      :openKeys.sync="openKeys"
       mode="inline"
       :theme="theme"
-      :inlineCollapsed="collapsed"
     >
       <template v-for="item in menuData">
-        <a-menu-item v-if="!item.children" :key="item.path">
+        <a-menu-item
+          v-if="!item.children"
+          :key="item.path"
+          @click="() => $router.push({ path: item.path, query: $route.query })"
+        >
           <a-icon v-if="item.meta.icon" :type="item.meta.icon" />
           <span>{{ item.meta.title }}</span>
         </a-menu-item>
@@ -34,23 +37,51 @@ export default {
   components: {
     "sub-menu": SubMenu
   },
+  watch: {
+    "$route.path": function(val) {
+      this.selectedKeys = this.selectedKeysMap[val];
+      this.openKeys = this.collapsed ? [] : this.openKeysMap[val];
+    },
+    collapsed(val) {
+      if (val) {
+        this.cacheOpenKeys = this.openKeys;
+        this.openKeys = [];
+      } else {
+        this.openKeys = this.cacheOpenKeys;
+      }
+    }
+  },
   data() {
+    this.selectedKeysMap = {};
+    this.openKeysMap = {};
     const menuData = this.getMenuData(this.$router.options.routes);
     return {
       collapsed: false,
-      list: [],
-      menuData
+      menuData,
+      selectedKeys: this.selectedKeysMap[this.$route.path],
+      openKeys: this.collapsed ? [] : this.openKeysMap[this.$route.path]
     };
   },
   methods: {
-    getMenuData(routes) {
+    getMenuData(routes = [], parentKeys = [], selectedKey) {
       const menuData = [];
       routes.forEach(item => {
         if (item.name && !item.hideInMenu) {
+          this.openKeysMap[item.path] = parentKeys;
+          this.selectedKeysMap[item.path] = [selectedKey || item.path];
           const newItem = { ...item };
           delete newItem.children;
           if (item.children && !item.hideChildrenInMenu) {
-            newItem.children = this.getMenuData(item.children);
+            newItem.children = this.getMenuData(item.children, [
+              ...parentKeys,
+              item.path
+            ]);
+          } else {
+            this.getMenuData(
+              item.children,
+              selectedKey ? parentKeys : [...parentKeys, item.path],
+              selectedKey || item.path
+            );
           }
           menuData.push(newItem);
         } else if (
@@ -58,10 +89,11 @@ export default {
           !item.hideChildrenInMenu &&
           item.children
         ) {
-          menuData.push(...this.getMenuData(item.children));
+          menuData.push(
+            ...this.getMenuData(item.children, [...parentKeys, item.path])
+          );
         }
       });
-      console.log(menuData);
       return menuData;
     },
     toggleCollapsed() {
--- cli/src/layouts/SubMenu.vue
+++ cli/src/layouts/SubMenu.vue
@@ -7,11 +7,15 @@
       /><span>{{ props.menuInfo.meta.title }}</span>
     </span>
     <template v-for="item in props.menuInfo.children">
-      <a-menu-item v-if="!item.children" :key="item.path">
-        <a-icon
-          v-if="props.menuInfo.meta.icon"
-          :type="props.menuInfo.meta.icon"
-        />
+      <a-menu-item
+        v-if="!item.children"
+        :key="item.path"
+        @click="
+          () =>
+            parent.$router.push({ path: item.path, query: parent.$route.query })
+        "
+      >
+        <a-icon v-if="item.meta.icon" :type="item.meta.icon" />
         <span>{{ item.meta.title }}</span>
       </a-menu-item>
       <sub-menu v-else :key="item.path" :menu-info="item" />
