2e7d125740354e8fc8c52f348c73a5505c8146ad
boxi <823998314@qq.com>
2019-10-14 19:06:43 +0800

    精细化，权限管理

--- cli/src/layouts/BasicLayout.vue
+++ cli/src/layouts/BasicLayout.vue
@@ -15,6 +15,7 @@
       <a-layout>
         <a-layout-header style="background: #fff; padding: 0">
           <a-icon
+            v-auth="['admin']"
             class="trigger"
             :type="collapsed ? 'menu-unfold' : 'menu-fold'"
             @click="collapsed = !collapsed"
@@ -29,6 +30,11 @@
         </a-layout-footer>
       </a-layout>
     </a-layout>
+    <!-- 权限验证方案1
+    <Authorized :authority="['admin']">
+      <SettingDrawer />
+    </Authorized>
+    -->
     <SettingDrawer />
   </div>
 </template>
--- cli/src/main.js
+++ cli/src/main.js
@@ -3,6 +3,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+//精细权限管理1
+import Authorized from "./components/Authorized";
+//精细权限管理2
+import Auth from "./directives/auth";
+
 import { Button, Layout, Icon, Drawer, Radio, Menu } from "ant-design-vue";
 Vue.config.productionTip = false;
 
@@ -12,6 +17,11 @@ Vue.use(Icon);
 Vue.use(Drawer);
 Vue.use(Radio);
 Vue.use(Menu);
+//权限管理的方法二：在用的地方， v-auth="['admin']" 例子：<div v-auth="['admin']"/>,只对第一次有用，当更换时，不能改变
+Vue.use(Auth);
+
+//权限管理的方法一：组建管理法，在用的地方，用<Authorized>嵌套例子： <Authorized :authority="['admin']"></Authorized>
+Vue.component("Authorized", Authorized);
 
 new Vue({
   router,
