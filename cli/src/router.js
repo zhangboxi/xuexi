3e13dda58696b9cbcc08577beff5cc183cf49350
boxi <823998314@qq.com>
2019-11-13 16:37:42 +0800

    修改打包东西

--- cli/src/router.js
+++ cli/src/router.js
@@ -9,7 +9,7 @@ import { check, isLogin } from "./utils/auth";
 Vue.use(Router);
 
 const router = new Router({
-  mode: "history",
+  // mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
